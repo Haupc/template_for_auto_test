6800a82e345a87c2d4b714830b173fbf02652157
haupc <haupc.uet@gmail.com>
2019-08-10 15:43:39 +0700

    update stringfilter

--- StringFilter.cs
+++ StringFilter.cs
@@ -1,20 +1,14 @@
-        /**
-         * String filter
-         * Require: 
-         */
         [TestMethod]
-        public async Task {repo_name}_Count_FilterBy{field_name}()
+        public async Task {repo_name}_FilterBy{field_name}ASC_ReturnValid()
         {
+            string paternToFilter = DbContext.{repo_name}.Select(l => l.{field_name}).FirstOrDefault().Substring(1);
+
             {repo_name}Filter filter = new {repo_name}Filter
             {
-                {field_name} = new StringFilter { Equal = /** Insert data overthere */ }
+                {field_name} = new StringFilter { StartsWith = paternToFilter },
             };
 
             var result = await repository.Count(filter);
-            var expected = await DbContext.{repo_name}
-                .Where(c => c.{field_name}, filter.{field_name})
-                .Select(c => c.{field_name})
-                .CountAsync();
-
+            var expected = await DbContext.{repo_name}.Where(l => l.{field_name}.StartsWith(paternToFilter)).Select(l => l.Id).CountAsync();
             Assert.AreEqual(expected, result);
         }
