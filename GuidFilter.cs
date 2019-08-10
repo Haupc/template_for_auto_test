20c77cdba9c709b3eb8143cd66c67b390f53e584
haupc <haupc.uet@gmail.com>
2019-08-10 11:11:26 +0700

    update GUID Filter

--- GuidFilter.cs
+++ GuidFilter.cs
@@ -1,16 +1,14 @@
         [TestMethod]
-        public async Task {repo_name}_ListFilterBy{Guid_filter_field}_ReturnTrue()
+        public async Task {repo_name}_ListFilterBy{filter_field}_ReturnTrue()
         {
-            {repo_name}Filter filter = new {repo_name}Filter()
+            {repo_name}Filter filter = new {repo_name}Filter
             {
-                // require_field = CreateGuid(code) (1)
-                {Guid_filter_field} = new GuidFilter() { Equal = CreateGuid(/** string patern */) }, //(2)
-                Skip = 0,
-                Take = 10
+                {filter_field} = new GuidFilter { Equal = CreateGuid(/** code patern */) },
             };
+
             var result = await repository.List(filter);
             var listCodes = string.Join(",", result.Select(r => r.Code));
-            var expected = await this.DbContext.{repo_name}.Where(c => c.{Guid_filter_field}.Equals(/** //(2) */) && c.{require_field}.Equals(/** Guid of require_field (1) */))
+            var expected = await this.DbContext.{repo_name}.Where(c => c.{filter_field}.Equals(/** code patern */))
                                                            .OrderBy(c => c.Code)
                                                            .Select(c => c.Code)
                                                            .Skip(0).Take(10)
